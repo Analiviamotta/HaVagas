da1f8e40e8ff4e75b8e55f75a05f0c1e64f95601
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-08-26 21:16:50 -0300

    feat: implements function when selected scholl

--- app/src/main/java/com/example/havagas/MainActivity.kt
+++ app/src/main/java/com/example/havagas/MainActivity.kt
@@ -1,14 +1,57 @@
 package com.example.havagas
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.view.isVisible
+import com.example.havagas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
+        amb.addCelBt.setOnClickListener {
+            amb.celLt.isVisible = true
+        }
+
+        amb.schoolTrainingSp.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedTraining = parent?.getItemAtPosition(position).toString()
+
+
+                when (selectedTraining) {
+                    "Fundamental", "Médio" -> {
+                        amb.schoolTrainingLt.isVisible = true
+                        amb.graduationYear.isVisible = true
+                    }
+                    "Graduação", "Especialização" -> {
+                        amb.schoolTrainingLt.isVisible = true
+                        amb.institutionYear.isVisible = true
+                        amb.graduationYear.isVisible = true
+                    }
+                    "Mestrado", "Doutorado" -> {
+                        amb.schoolTrainingLt.isVisible = true
+                        amb.institutionYear.isVisible = true
+                        amb.graduationYear.isVisible = true
+                        amb.thesisTitle.isVisible = true
+                        amb.advisorTitle.isVisible = true
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+
+
     }
-}
+}}
