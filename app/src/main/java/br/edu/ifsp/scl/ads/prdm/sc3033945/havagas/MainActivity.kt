0879929ef620ee71c8820fc27c486392a0d8cb54
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-09-03 21:14:07 -0300

    feat(main-activity): configure main activity to use view binding

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/havagas/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033945/havagas/MainActivity.kt
@@ -1,12 +1,23 @@
 package br.edu.ifsp.scl.ads.prdm.sc3033945.havagas
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.prdm.sc3033945.havagas.databinding.ActivityMainBinding
 
+// o AppCompatActivity é o que faz o main activity ser uma tela
 class MainActivity : AppCompatActivity() {
+    // val é uma variavel de leitura apenas
+    // o by lazy significa que o que está dentro da chaves só será criado uma vez,
+    // depois fica em cache
+
+    private val amb : ActivityMainBinding by lazy {
+        // pega o layout correspondente a ActivityMainBinding
+        // como saber se um layout corresponde a class? Existe um padrao de nomenclatura, o nome da
+        // class corresponse ao nome do xml de seu layout em PascalCase acompanhado da palavra
+        // Binding
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
